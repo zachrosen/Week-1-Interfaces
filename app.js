7c190b8f9c5fa26e12811e771ffdc034bc54a950
Zach R <zachrosen23@Zachs-MacBook-Air.local>
2016-05-20 10:52:43 -0700

    Shows color as value with text

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var app;
 (function (app) {
     var vehicles = [];
-    vehicles.push(new app.models.Car('Dodge', 'Challenger', 'Black', 2, 2));
+    vehicles.push(new app.models.Car('Dodge', 'Challenger', 'Magenta', 2, 2));
     vehicles.push(new app.models.Car('Ferrari', 'Modena', 'Red', 2, 2));
     vehicles.push(new app.models.Car('Chevrolet', 'Volt', 'Purple', 4, 5, false));
-    vehicles.push(new app.models.Truck('Ram', 'SuperCab', 'Navy Blue', 4, 5, true));
+    vehicles.push(new app.models.Truck('Ram', 'SuperCab', 'Navy', 4, 5, true));
     displayVehicles();
     function displayVehicles() {
         var elemString = "";
         vehicles.forEach(function (v, index) {
-            elemString += "<tr>\n    <td>" + v.make + "</td>\n    <td>" + v.model + "</td>\n    <td>" + v.color + "</td>\n    <td>" + v.numDoors + "</td>\n    <td>" + v.numSeats + "</td>\n    <td>N/A</td>\n    <td class=\"hcenter\"> <button class=\"btn btn-danger\" onclick=\"app.deleteVehicle(" + index + ")\">Delete</button> </td>\n    </tr>\n    ";
+            elemString += "<tr>\n    <td class=\"hcenter\">" + v.make + "</td>\n    <td class=\"hcenter\">" + v.model + "</td>\n    <td class = \"hcenter\" style=\"background-color:" + v.color + "\">" + v.color + "</td>\n    <td class=\"hcenter\">" + v.numDoors + "</td>\n    <td class=\"hcenter\">" + v.numSeats + "</td>\n    <td class=\"hcenter\">N/A</td>\n    <td class=\"hcenter\"> <button class=\"btn btn-danger\" onclick=\"app.deleteVehicle(" + index + ")\">Delete</button> </td>\n    </tr>\n    ";
         });
         $('#vehicle-display').html(elemString);
     }
@@ -19,15 +19,16 @@ var app;
     }
     app.deleteVehicle = deleteVehicle;
     function startVehicleCreate() {
-        $('#create-header').html('<span>Choose a vehicle type: </span><select class="form-control" style="max-width: 10%; display: inline-block;" id="create-vehicle-type" onchange="app.chooseVehicleStyle()"><option disabled selected>---</option><option value = "Car">Car</option><option value = "Truck">Truck</option></select><span> or </span><a>Cancel</a>');
+        $('#create-header').html("\n          <span>Choose a vehicle type: </span>\n          <select class=\"form-control\" style=\"max-width: 10%; display: inline-block\";\n          id=\"create-vehicle-type\"\n          onchange=\"app.chooseVehicleStyle()\">\n          <option disabled selected>---</option>\n          <option value = \"Car\">Car</option>\n          <option value = \"Truck\">Truck</option></select>\n          <span> or </span><a>Cancel</a>");
     }
     app.startVehicleCreate = startVehicleCreate;
     function chooseVehicleStyle() {
         var type = $('#create-vehicle-type').val();
         if (type === 'Car') {
-            $('#create-header').html("\n      <div class=\"row\">\n      <div class=\"form-group\">\n        <label>Car Make</label>\n        <input id=\"car-make-input\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Car Model</label>\n        <input id=\"car-model-input\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Color</label>\n        <input id=\"car-color-input\" type=\"color\" class=\"form-control\" />\n      </div>\n    </div>\n    <div class=\"row\">\n      <div class=\"form-group\">\n        <label>Number of Doors</label>\n        <input id=\"car-numDoors-input\" type=\"number\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Number of Seats</label>\n        <input id=\"car-numSeats-input\" type=\"number\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Is it Cool?</label>\n        <select id=\"car-isCool-input\" class=\"form-control\">\n          <option value=\"true\" selected>Yes</option>\n          <option value=\"false\">No</option>\n        </select>\n      </div>\n    </div>\n    <div class=\"row\">\n      <button class=\"btn btn-info\" onclick=\"app.createCar()\">Submit</button>\n    </div>\n      ");
+            $('#create-header').html("\n      <div class=\"row\">\n      <div class=\"form-group\">\n        <label>Car Make</label>\n        <input id=\"car-make-input\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Car Model</label>\n        <input id=\"car-model-input\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Color</label>\n        <input id=\"car-color-input\" type=\"color\" class=\"form-control\" />\n      </div>\n    </div>\n    <div class=\"row\">\n      <div class=\"form-group\">\n        <label>Number of Doors</label>\n        <input id=\"car-numDoors-input\" type=\"number\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Number of Seats</label>\n        <input id=\"car-numSeats-input\" type=\"number\" class=\"form-control\" />\n      </div>\n      <div class=\"form-group\">\n        <label>Is it Cool?</label>\n        <select id=\"car-isCool-input\" class=\"form-control\">\n          <option value=\"true\" selected>Yes</option>\n          <option value=\"false\">No</option>\n        </select>\n      </div>\n    </div>\n    <div class=\"row\">\n      <button class=\"btn btn-success\" onclick=\"app.createCar()\">Submit</button>\n      <button class=\"btn btn-warning\" onclick=\"app.returnToBeg()\">Go Back</button>\n    </div>\n      ");
         }
         else if (type === 'Truck') {
+            $('#create-header').html("\n    <div class=\"row\">\n    <div class=\"form-group\">\n      <label>Truck Make</label>\n      <input id=\"truck-make-input\" class=\"form-control\" />\n    </div>\n    <div class=\"form-group\">\n      <label>Truck Model</label>\n      <input id=\"truck-model-input\" class=\"form-control\" />\n    </div>\n    <div class=\"form-group\">\n      <label>Color</label>\n      <input id=\"truck-color-input\" type=\"color\" class=\"form-control\" />\n    </div>\n  </div>\n  <div class=\"row\">\n    <div class=\"form-group\">\n      <label>Number of Doors</label>\n      <input id=\"truck-numDoors-input\" type=\"number\" class=\"form-control\" />\n    </div>\n    <div class=\"form-group\">\n      <label>Number of Seats</label>\n      <input id=\"truck-numSeats-input\" type=\"number\" class=\"form-control\" />\n    </div>\n    <div class=\"form-group\">\n      <label>Is it Cool?</label>\n      <select id=\"truck-isCool-input\" class=\"form-control\">\n        <option value=\"true\" selected>Yes</option>\n        <option value=\"false\">No</option>\n      </select>\n    </div>\n  </div>\n  <div class=\"row\">\n    <button class=\"btn btn-success\" onclick=\"app.createTruck()\">Submit</button>\n    <button class=\"btn btn-warning\" onclick=\"app.returnToBeg()\">Go Back</button>\n  </div>\n");
         }
     }
     app.chooseVehicleStyle = chooseVehicleStyle;
@@ -38,7 +39,7 @@ var app;
         var numDoors = $('#car-numDoors-input').val();
         var numSeats = $('#car-numSeats-input').val();
         var isCool = $('#car-isCool-input').val();
-        var tempCar = new app.models.Car(make, model, numDoors, numSeats, isCool);
+        var tempCar = new app.models.Car(make, model, color, numDoors, numSeats, isCool);
         vehicles.push(tempCar);
         displayVehicles();
         $('#car-make-input').val('');
@@ -49,4 +50,27 @@ var app;
         $('#car-isCool-input').val('');
     }
     app.createCar = createCar;
+    function createTruck() {
+        var make = $('#truck-make-input').val();
+        var model = $('#truck-model-input').val();
+        var color = $('#truck-color-input').val();
+        var numDoors = $('#truck-numDoors-input').val();
+        var numSeats = $('#truck-numSeats-input').val();
+        var isCool = $('#truck-isCool-input').val();
+        var tempTruck = new app.models.Truck(make, model, color, numDoors, numSeats, isCool);
+        vehicles.push(tempTruck);
+        displayVehicles();
+        $('#truck-make-input').val('');
+        $('#truck-model-input').val('');
+        $('#truck-color-input').val('');
+        $('#truck-numDoors-input').val('');
+        $('#truck-numSeats-input').val('');
+        $('#truck-isCool-input').val('');
+    }
+    app.createTruck = createTruck;
+    function returnToBeg() {
+        $('#create-header').html("<a onclick= \"app.startVehicleCreate()\">Add a Vehicle</a>");
+        $('#go-back').html("<a onclick= \"app.startVehicleCreate()\">Add a Vehicle</a>");
+    }
+    app.returnToBeg = returnToBeg;
 })(app || (app = {}));
--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 namespace app {
     let vehicles: IVehicle[] = [];
-    vehicles.push(new models.Car('Dodge', 'Challenger', 'Black', 2, 2));
+    vehicles.push(new models.Car('Dodge', 'Challenger', 'Magenta', 2, 2));
     vehicles.push(new models.Car('Ferrari', 'Modena', 'Red', 2, 2));
     vehicles.push(new models.Car('Chevrolet', 'Volt', 'Purple', 4, 5, false));
-    vehicles.push(new models.Truck('Ram', 'SuperCab', 'Navy Blue', 4, 5, true));
+    vehicles.push(new models.Truck('Ram', 'SuperCab', 'Navy', 4, 5, true));
 
     displayVehicles();
 
@@ -11,12 +11,12 @@ namespace app {
         let elemString = "";
         vehicles.forEach((v, index) => {
             elemString += `<tr>
-    <td>${v.make}</td>
-    <td>${v.model}</td>
-    <td>${v.color}</td>
-    <td>${v.numDoors}</td>
-    <td>${v.numSeats}</td>
-    <td>N/A</td>
+    <td class="hcenter">${v.make}</td>
+    <td class="hcenter">${v.model}</td>
+    <td class = "hcenter" style="background-color:${v.color}">${v.color}</td>
+    <td class="hcenter">${v.numDoors}</td>
+    <td class="hcenter">${v.numSeats}</td>
+    <td class="hcenter">N/A</td>
     <td class="hcenter"> <button class="btn btn-danger" onclick="app.deleteVehicle(${index})">Delete</button> </td>
     </tr>
     `
@@ -30,7 +30,15 @@ namespace app {
         }
 
     export function startVehicleCreate() {
-        $('#create-header').html('<span>Choose a vehicle type: </span><select class="form-control" style="max-width: 10%; display: inline-block;" id="create-vehicle-type" onchange="app.chooseVehicleStyle()"><option disabled selected>---</option><option value = "Car">Car</option><option value = "Truck">Truck</option></select><span> or </span><a>Cancel</a>'
+        $('#create-header').html(`
+          <span>Choose a vehicle type: </span>
+          <select class="form-control" style="max-width: 10%; display: inline-block";
+          id="create-vehicle-type"
+          onchange="app.chooseVehicleStyle()">
+          <option disabled selected>---</option>
+          <option value = "Car">Car</option>
+          <option value = "Truck">Truck</option></select>
+          <span> or </span><a>Cancel</a>`
         );
     }
 
@@ -71,12 +79,51 @@ namespace app {
       </div>
     </div>
     <div class="row">
-      <button class="btn btn-info" onclick="app.createCar()">Submit</button>
+      <button class="btn btn-success" onclick="app.createCar()">Submit</button>
+      <button class="btn btn-warning" onclick="app.returnToBeg()">Go Back</button>
     </div>
       `)
+
         } else if (type === 'Truck') {
           //TODO: add create truck form
           //TODO: add createTruck() method
+          $('#create-header').html(`
+    <div class="row">
+    <div class="form-group">
+      <label>Truck Make</label>
+      <input id="truck-make-input" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label>Truck Model</label>
+      <input id="truck-model-input" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label>Color</label>
+      <input id="truck-color-input" type="color" class="form-control" />
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group">
+      <label>Number of Doors</label>
+      <input id="truck-numDoors-input" type="number" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label>Number of Seats</label>
+      <input id="truck-numSeats-input" type="number" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label>Is it Cool?</label>
+      <select id="truck-isCool-input" class="form-control">
+        <option value="true" selected>Yes</option>
+        <option value="false">No</option>
+      </select>
+    </div>
+  </div>
+  <div class="row">
+    <button class="btn btn-success" onclick="app.createTruck()">Submit</button>
+    <button class="btn btn-warning" onclick="app.returnToBeg()">Go Back</button>
+  </div>
+`)
         }
     }
 
@@ -88,7 +135,9 @@ namespace app {
   let numSeats = $('#car-numSeats-input').val();
   let isCool = $('#car-isCool-input').val();
 
-  let tempCar = new models.Car(make, model, numDoors, numSeats, isCool);
+
+
+  let tempCar = new models.Car(make, model, color, numDoors, numSeats, isCool);
   vehicles.push(tempCar);
   displayVehicles();
 
@@ -99,5 +148,29 @@ namespace app {
   $('#car-numDoors-input').val('');
   $('#car-numSeats-input').val('');
   $('#car-isCool-input').val('');
+}
+export function createTruck() {
+let make = $('#truck-make-input').val();
+let model = $('#truck-model-input').val();
+let color = $('#truck-color-input').val();
+let numDoors = $('#truck-numDoors-input').val();
+let numSeats = $('#truck-numSeats-input').val();
+let isCool = $('#truck-isCool-input').val();
+
+let tempTruck = new models.Truck(make, model, color, numDoors, numSeats, isCool);
+vehicles.push(tempTruck);
+displayVehicles();
+
+//input.value = '';
+$('#truck-make-input').val('');
+$('#truck-model-input').val('');
+$('#truck-color-input').val('');
+$('#truck-numDoors-input').val('');
+$('#truck-numSeats-input').val('');
+$('#truck-isCool-input').val('');
+}
+export function returnToBeg(){
+$('#create-header').html(`<a onclick= "app.startVehicleCreate()">Add a Vehicle</a>`)
+$('#go-back').html(`<a onclick= "app.startVehicleCreate()">Add a Vehicle</a>`)
 }
 }
